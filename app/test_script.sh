22d61e4c0fa4991bbf222bc6b1ba04946f0654f7
holee.lmh <lmh.leemanho@gmail.com>
2023-08-29 09:57:05 +0800

    update config example and test script

--- app/test_script.sh
+++ app/test_script.sh
@@ -15,7 +15,7 @@ fi
 
 
 
-echo "Running: python stock_analyser.py -f=./configs/2stocks.txt -s=2022-08-01 -e=2023-08-01 -g=../../unit_test_result/ -v=../../unit_test_result"
+echo "Running: python stock_analyser.py -f=./configs/2stocks.txt -s=2022-08-01 -e=2023-08-01 -g=../../unit_test_result/ -v=../../unit_test_result ..."
 python stock_analyser.py -f=./configs/2stocks.txt -s=2022-08-01 -e=2023-08-01 -g=../../unit_test_result/ -v=../../unit_test_result
 
 if [ $? -ne 0 ]; then
@@ -27,7 +27,7 @@ echo "Testing backtest.py ..."
 
 
 
-echo "Running backtest.py -t=pdd -s=2022-08-01 -e=2023-08-16 -c=10000 -o=no -v=../../unit_test_result -g=../../unit_test_result"
+echo "Running backtest.py -t=pdd -s=2022-08-01 -e=2023-08-16 -c=10000 -o=no -v=../../unit_test_result -g=../../unit_test_result ..."
 python backtest.py -t=pdd -s=2022-08-01 -e=2023-08-16 -c=10000 -o=no -v=../../unit_test_result -g=../../unit_test_result
 
 if [ $? -ne 0 ]; then
@@ -36,14 +36,15 @@ if [ $? -ne 0 ]; then
 fi
 
 python backtest.py -j=./configs/backtest_config_example.json
-echo "Running: python backtest.py -j=./configs/backtest_config_example.json"
+echo "Running: python backtest.py -j=./configs/backtest_config_example.json ..."
 if [ $? -ne 0 ]; then
     echo "Error occurred in 1st test"
     exit 1
 fi
 
+echo "Running: python backtest.py -j=./configs/list_of_stock_1yr_config.json ..."
 python backtest.py -j=./configs/list_of_stock_1yr_config.json
-echo "Running: python backtest.py -j=./configs/list_of_stock_1yr_config.json"
+
 
 if [ $? -ne 0 ]; then
     echo "Error occurred in 1st test"
